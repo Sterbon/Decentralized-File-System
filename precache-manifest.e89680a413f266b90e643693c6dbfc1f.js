b5ec4d1a486ecb51ad617a0414da492dd5f8ac37
Utsav Talwar <talwar.utsav@gmail.com>
2019-07-11 18:23:27 +0530

    Updates

---
+++ precache-manifest.e89680a413f266b90e643693c6dbfc1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0135fe7ca1f27edb5024db583f4fe8",
+    "url": "/auth.or/index.html"
+  },
+  {
+    "revision": "6cd6ca64dcdca0307223",
+    "url": "/auth.or/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "ca7ad459b700f1f760bb",
+    "url": "/auth.or/static/css/main.96b17872.chunk.css"
+  },
+  {
+    "revision": "6cd6ca64dcdca0307223",
+    "url": "/auth.or/static/js/2.f9a7bd86.chunk.js"
+  },
+  {
+    "revision": "ca7ad459b700f1f760bb",
+    "url": "/auth.or/static/js/main.e7c9d4d6.chunk.js"
+  },
+  {
+    "revision": "58c47304851577694652",
+    "url": "/auth.or/static/js/runtime~main.99bdf044.js"
+  }
+]);
