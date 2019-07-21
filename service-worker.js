cb7b99e3599861505d6b8f2a6e25acb510f60fc1
Sterbon <talwar.utsav@gmail.com>
2019-07-21 20:27:05 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/author/precache-manifest.04a5a56d46717c8938e19b9232ab8c54.js"
+  "/author/precache-manifest.cf4cc45e44aaa22f4e5bada9bb00946d.js"
 );
 
 self.addEventListener('message', (event) => {
