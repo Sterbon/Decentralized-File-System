5d688ada4fefd87074b06fdc1d8923863b2c163f
Utsav Talwar <talwar.utsav@gmail.com>
2019-07-22 16:22:46 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/author/precache-manifest.b4a93f787b61c148c8b25f26965ec78c.js"
+  "/author/precache-manifest.317e5d7ba9d2cf3a49cefd0537e6aa34.js"
 );
 
 self.addEventListener('message', (event) => {
